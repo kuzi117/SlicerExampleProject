eaf70101659c81b52aa52866abb472828b9d66b9
Braedy Kuzma <braedy@ualberta.ca>
2019-03-30 13:11:39 -0600

    Implement initBoard.

--- src/xos/board.c
+++ src/xos/board.c
@@ -1,8 +1,15 @@
-#include <xos/board.h>
 #include "xos/board.h"
 
-void initGame(XOGame *game) {
+#include <stddef.h>
 
+void initGame(XOGame *game) {
+  game->turn = TURN_X;
+
+  for (size_t i = 0; i < 3; ++i) {
+    for (size_t j = 0; j < 3; ++j) {
+      game->tiles[i][j] = TILE_NONE;
+    }
+  }
 }
 
 bool playPosition(XOGame *game, unsigned int xPos, unsigned int yPos, Tile tile) {
