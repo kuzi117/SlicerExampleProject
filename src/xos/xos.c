e3db60d02d36567b559477a5af1c447baa4b2368
Braedy Kuzma <braedy@ualberta.ca>
2019-03-31 13:20:32 -0600

    Advance turns.

--- src/xos/xos.c
+++ src/xos/xos.c
@@ -7,11 +7,13 @@
 void playXOs() {
   // Init game.
   XOGame game;
-  initGame(&game);
+  XOGame *gp = &game;
+  initGame(gp);
 
   // Make move.
   unsigned int x, y;
   for (size_t i = 0; i < 9; ++i) {
     scanf("%d %d", &x, &y);
+    advanceTurn(gp);
   }
 }
