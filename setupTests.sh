6b75a1f456eae7475fb2c19799b3e39722224b7e
Braedy Kuzma <braedy@ualberta.ca>
2019-03-31 23:47:07 -0600

    Build test slice.
    Fingers crossed.

--- setupTests.sh
+++ setupTests.sh
@@ -7,16 +7,29 @@ if [ -z "${since}" ]; then
   exit 1
 fi
 
-# Make the slicer tools.
+# Hold onto base dir and get base commit so we always know where to go.
+projName=`basename ${PWD}`
+base=`pwd`
+curSHA=`git log --format="%h" | head -n 1`
+oldSHA="${since}"
+
+# TODO Can't depend on the system opt version, need to check if opt version
+opt="${LLVM_BIN}/opt"
+
+# Get the slicer.
 git clone https://github.com/kuzi117/slicer.git
 cd slicer
+slicer=`pwd`
+
+# Make the slicer tools.
 cmake .
 make -j4
 
 # Set up executables.
-differ="${PWD}/src/differ/differ"
-producer="${PWD}/src/sliceProducer/sliceProducer"
-builder="${PWD}/lib/cgBuilder/libcgBuilder.so"
+differ="${slicer}/src/differ/differ"
+producer="${slicer}/src/sliceProducer/sliceProducer"
+analyser="${slicer}/lib/cgBuilder/libcgBuilder.so"
+builder="${slicer}/sh/compileAndLinkLL.sh"
 
 # Check for existence.
 if [ ! -f ${differ} ]; then
@@ -35,5 +48,71 @@ if [ ! -f ${differ} ]; then
   echo "Call graph analyser didn't exist at assumed path."
   exit 1
 else
-  echo "Call graph analyser: ${builder}"
+  echo "Call graph analyser: ${analyser}"
 fi
+
+# Go back to base.
+cd "${base}"
+
+# Set up for building versions.
+src="${base}/src"
+inc="${base}/include"
+buildDir="${base}/sliceBuilds"
+projDir="${buildDir}/${projName}"
+oldBC="${projDir}/${oldSHA}.bc"
+curBC="${projDir}/${curSHA}.bc"
+
+# Build for cur version.
+curCFiles=\
+`find "${src}" -path "${src}/tests/CuTest.c" -prune \
+            -o -path "${src}/tests/unslicedTests.c" -prune \
+            -o -path "${src}/tests/testSlice.c" -prune \
+            -o -path "${src}/test.c" -prune \
+            -o -type f -name *.c -print`
+"${builder}" "${projName}" "${curSHA}" "${buildDir}" "${inc}" ${curCFiles}
+
+# Checkout old version.
+git checkout "${oldSHA}"
+oldCFiles=\
+`find "${src}" -path "${src}/tests/CuTest.c" -prune \
+            -o -path "${src}/tests/unslicedTests.c" -prune \
+            -o -path "${src}/tests/testSlice.c" -prune \
+            -o -path "${src}/test.c" -prune \
+            -o -type f -name *.c -print`
+"${builder}" "${projName}" "${oldSHA}" "${buildDir}" "${inc}" ${oldCFiles}
+
+# Reset the directory to the original SHA to let tools and tests run on correct source version.
+git checkout "${curSHA}"
+
+# Do the diff.
+diffFile="${buildDir}/diff.txt"
+"${differ}" "${oldBC}" "${curBC}" > "${diffFile}"
+echo "Diff file:"
+cat "${diffFile}"
+
+# Do the affected callgraph analysis.
+affFile="${buildDir}/aff.txt"
+"${opt}" "${curBC}" \
+  -load="${analyser}" -cgBuilder \
+  -track="${diffFile}" -dest="${affFile}" \
+  -calls \
+  > /dev/null
+echo "Affected file:"
+cat "${affFile}"
+
+# Do the affected callgraph analysis.
+testFile="${buildDir}/tests.txt"
+"${opt}" "${curBC}" \
+  -load="${analyser}" -cgBuilder \
+  -track="${affFile}" -dest="${testFile}" \
+  -tests \
+  > /dev/null
+echo "Tests file:"
+cat "${testFile}"
+
+# Produce the slice tests file.
+sliceFile="${src}/tests/testSlice.c"
+"${producer}" "${testFile}" "${sliceFile}"
+echo "Slice file:"
+cat "${sliceFile}"
+
